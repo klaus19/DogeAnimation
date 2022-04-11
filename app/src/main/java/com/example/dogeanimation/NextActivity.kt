6c65e1c61f465bc9926cfef88065fe7f7c08e63b
Tejas khartude <tejas.khartude@clariontechnologies.co.in>
2022-04-11 10:34:53 +0530

    Added a new View in the XML file

--- app/src/main/java/com/example/dogeanimation/NextActivity.kt
+++ app/src/main/java/com/example/dogeanimation/NextActivity.kt
@@ -1,6 +1,8 @@
 package com.example.dogeanimation
 
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dogeanimation.databinding.NextLayoutBinding
 
@@ -14,6 +16,41 @@ class NextActivity: AppCompatActivity() {
         binding = NextLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        binding.btnRepeat.setOnClickListener {
+            repeatMode()
+        }
+
+
+    }
+
+    private fun repeatMode() {
+
+        //finding the height of the screen
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val height = displayMetrics.heightPixels.toFloat()
+
+        //1 - Creating a ValueAnimator
+
+        val animator = ValueAnimator.ofFloat(0f,-height)
+
+        animator.addUpdateListener {
+            val value = it.animatedValue as Float
+            binding.btnRepeat.translationY = value
+        }
+
+        //2 Repeat repeatMode
+        animator.repeatMode = ValueAnimator.REVERSE
+
+        //3
+        animator.repeatCount=3
+
+        //4
+        animator.duration = 1000L
+
+        //5
+        animator.start()
 
     }
 }
